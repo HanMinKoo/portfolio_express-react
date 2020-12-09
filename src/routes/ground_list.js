a0d04fcaf252839e64dd2cba2209ea422632f7ad
HanMinKoo <alsrnalsrn94@naver.com>
2020-12-09 18:09:06 +0900

    201209 Add ground_list responsive

--- src/routes/ground_list.js
+++ src/routes/ground_list.js
@@ -1,5 +1,5 @@
 import React, {useState,useEffect} from 'react';
-import '../css/ground.css';
+import '../css/ground_list.css';
 
 import {Link} from 'react-router-dom';
 import {fetchGroundImg,fetchGroundInfoAndTimeList} from '../components/fetchGroundData.js';
@@ -25,14 +25,16 @@ const initJSX = (groundListData,groundImgData) => {
                 <div className="groundList_content" key={i}>
                     <Link to={path}>
                         <img className="groundImg" src={imgSrc}/>
-                        <ul className="groundInfo_NameLocation">
-                            <li><h2>{name}</h2></li>
-                            <li className="groundInfo"><h3>{location} </h3></li>
-                        </ul>
-                        <ul className="groundInfo_TimePrice">
-                            <li className="groundInfo"><h3>{use_time} </h3></li>
-                            <li className="groundInfo"><h3>{price} 만원</h3></li> 
-                        </ul>
+                        <div className="groundInfo_Text">
+                            <ul className="groundInfo_NameLocation">
+                                <li><h2>{name}</h2></li>
+                                <li className="groundInfo"><h3>{location} </h3></li>
+                            </ul>
+                            <ul className="groundInfo_TimePrice">
+                                <li className="groundInfo"><h3>{use_time} </h3></li>
+                                <li className="groundInfo"><h3>{price} 만원</h3></li> 
+                            </ul>
+                        </div>
                     </Link>
                 </div>
         }
