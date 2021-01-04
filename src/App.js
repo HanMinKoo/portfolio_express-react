68e459db4cf7573b1ef7199351f4ff1a46e8b612
HanMinKoo <alsrnalsrn94@naver.com>
2021-01-04 18:18:44 +0900

    210104 refator nav css change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import './App.css';
 
 import Footer from './components/footer';
 import Nav from './components/nav';
